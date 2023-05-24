f0530fe85639edce6961cd77d9d1b56e8f5a061b
awwwrina <88673650+awwwrina@users.noreply.github.com>
2023-05-24 23:30:56 +0300

    add date picker

---
+++ client/src/components/forms/DateInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import moment from "moment";
+import DatePicker from "react-datepicker";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import ru from "date-fns/locale/ru";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+export const DateInput = ({setFieldValue}) => {
+	const [date, setDate] = useState(null);
+	registerLocale("ru", ru);
+	return (
+		<DatePicker
+			locale="ru"
+			selected={date}
+			onChange={(date) => {
+				setDate(date);
+				setFieldValue("birthDate", date);
+			}}
+			placeholderText={"Выберите дату"}
+			isClearable={false}
+			showYearDropdown={true}
+			dateFormatCalendar="MMMM"
+			dropdownMode="select"
+			minDate={moment().subtract(100, "years")._d}
+			maxDate={moment().subtract(18, "years")._d}
+		/>
+	);
+};
