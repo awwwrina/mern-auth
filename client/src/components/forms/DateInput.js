caec35c38a667891916bfa84dbec89b7a8c32044
awwwrina <88673650+awwwrina@users.noreply.github.com>
2023-05-25 22:52:36 +0300

    create DateInput component

--- client/src/components/forms/DateInput.js
+++ client/src/components/forms/DateInput.js
@@ -1,29 +1,18 @@
-import React, { useState } from "react";
-import moment from "moment";
-import DatePicker from "react-datepicker";
-import { registerLocale, setDefaultLocale } from "react-datepicker";
-import ru from "date-fns/locale/ru";
-import "react-datepicker/dist/react-datepicker.css";
+import { Field } from "formik";
+import { Label } from "./Label";
+import { Errors } from "./Errors";
+import { DatePickerComponent } from "./DatePickerComponent";
 
-
-export const DateInput = ({setFieldValue}) => {
-	const [date, setDate] = useState(null);
-	registerLocale("ru", ru);
+export const DateInput = ({ name, errors, label, touched, setFieldValue }) => {
 	return (
-		<DatePicker
-			locale="ru"
-			selected={date}
-			onChange={(date) => {
-				setDate(date);
-				setFieldValue("birthDate", date);
-			}}
-			placeholderText={"Выберите дату"}
-			isClearable={false}
-			showYearDropdown={true}
-			dateFormatCalendar="MMMM"
-			dropdownMode="select"
-			minDate={moment().subtract(100, "years")._d}
-			maxDate={moment().subtract(18, "years")._d}
-		/>
+		<>
+			<Label name={name} label={label} />
+			<Field
+				name={name}
+				as={DatePickerComponent}
+				setFieldValue={setFieldValue}
+			/>
+			<Errors errors={errors} touched={touched} name={name} />
+		</>
 	);
 };
